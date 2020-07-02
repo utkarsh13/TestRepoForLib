1259fe81cd6688d874aebe654789a5d787d50d79
Utkarsh Rastogi <utkarsh.013@gmail.com>
2020-07-02 23:08:42 +0530

    Integrating test library

--- app/src/main/java/com/utkarshr/testlibapp/MainActivity.kt
+++ app/src/main/java/com/utkarshr/testlibapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.utkarshr.testlibapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.utkarshr.testlibrary.TestClass
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        TestClass.toast(this)
+
     }
 }
